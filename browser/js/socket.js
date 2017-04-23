5c3d7105e2f48237ca0fb38515caacb7e8736d8d
gil lawson <gewl@users.noreply.github.com>
2017-04-22 22:27:42 -0400

    configuring for sockets

--- browser/js/socket.js
+++ browser/js/socket.js
@@ -7,14 +7,6 @@ export default class Socket {
 			let { piece, x, y } = moveObj
 			setTimeout(() => this.game.move(piece, x, y), 1000)
 		})
-
-		// socket.on('winEvent', board => {
-		// 	setTimeout(() => this.game.onWin(board), 1000)
-		// })
-
-		// socket.on('loseEvent', board => {
-		// 	setTimeout(() => this.game.onLose(board), 1000)
-		// })
 	}
 
 	sendGameState(gameState) {
